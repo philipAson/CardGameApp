a2e5a225871eaecdcadf64802cb955f716623272
Philip Andersson <philip.andersson.1991@hotmail.com>
2022-12-02 09:37:09 +0100

    final commit

--- app/src/main/java/com/example/cardgameapp/WinLooseActivity.kt
+++ app/src/main/java/com/example/cardgameapp/WinLooseActivity.kt
@@ -1,19 +1,26 @@
 package com.example.cardgameapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcelable
+import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class WinLooseActivity : AppCompatActivity() {
 
     lateinit var winOrLooseTextView: TextView
+    lateinit var testImageView: ImageView
 
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_win_loose)
 
+        testImageView = findViewById(R.id.imageView3)
 
         val rightAnswer = intent.getIntExtra("rightAnswer", 0)
 
@@ -25,16 +32,12 @@ class WinLooseActivity : AppCompatActivity() {
             winOrLooseTextView.text = "You Loose"
         }
 
-
         val playAgainButton = findViewById<Button>(R.id.playAgainButton)
         playAgainButton.text = "Play Again?"
 
         playAgainButton.setOnClickListener {
+
             finish()
         }
-
-
-
     }
-
 }
